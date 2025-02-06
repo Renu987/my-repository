f0e6e75709ac14f9d57b7fa0ec0b1b714b7ebe29
Renu987 <renuka.yamala2422@gmail.com>
2025-02-06 14:02:23 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull renuka2422/simple-python-flask-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 renuka2422/simple-python-flask-app
